2da462ea4c7a9952007067e1712b4c32fc0d1b10
yaswanth029 <yashxtension@gmail.com>
2026-05-01 18:44:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const inputbox = document.getElementById("input-box");
+const listcont = document.getElementById("list-container");
+
+function addTask()
+{
+    if(inputbox.value === '')
+    {
+        alert('you must write');
+    }
+    else{
+        let li = document.createElement('li');
+        li.innerHTML = inputbox.value;
+        listcont.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        li.append(span);
+    }
+    inputbox.value = '';
+    savedata();
+}
+
+listcont.addEventListener("click",function(e)
+{
+    if(e.target.tagName === "LI")
+    {
+        e.target.classList.toggle('checked');
+    }
+    else if(e.target.tagName === "SPAN")
+    {
+        e.target.parentElement.remove();
+    }
+},false);
+
+function savedata()
+{
+    localStorage.setItem("data",listcont.innerHTML)
+}
+
+function showlist()
+{
+    let saved = localStorage.getItem("data");
+    if(saved)
+    {
+    listcont.innerHTML = saved;
+    }
+}
+showlist();
+
